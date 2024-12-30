6cbeb789d0f9b3ff770b08357984e9e4efd9b8d2
Ritesh Kumar <ritesh.prog@gmail.com>
2024-12-30 09:54:30 +0530

    Add project files.

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,9 @@
+namespace MagicVilla_VillaAPI
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
